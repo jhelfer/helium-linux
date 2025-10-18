e8a7442e60346163a80f59fe243dbee7169dab7f
jj <log@riseup.net>
2025-10-18 13:02:06 +0200

    ci: move disk space cleanup to separate step (#82)

--- .github/scripts/import-cache.sh
+++ .github/scripts/import-cache.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-# disk space on free runners is limited, let's make some
-# space while waiting for build tree extraction to finish
-sudo rm -rf /usr/local/lib/android \
-            /usr/local/.ghcup \
-            /usr/lib/jvm \
-            /usr/lib/google-cloud-sdk \
-            /usr/lib/dotnet \
-            /usr/share/swift
-
 _base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && cd ../.. && pwd)"
 _cache_tar="$_base_dir/.github/cache/build-cache-$ARCH.tar.zst"
 
